3ba21c30b8271ae32ac1528c4650c3f0572b9b4e
75776140 <madiyar03633@gmail.com>
2023-12-07 18:32:01 +0600

    feat: add triangular_number.py

---
+++ triangular_number.py
@@ -0,0 +1,15 @@
+import math
+
+
+def is_perfect_square(x):
+    sqrt_value = math.sqrt(x)
+    return int(sqrt_value) ** 2 == x
+
+number = int(input)
+testing_number = 1 + 8 * number
+if is_perfect_square(testing_number):
+    print("It is a triangular number")
+else:
+    print("It is not a triangular number")
+
+# 45 is a triangular number 
